e65795d3d4f455dd302a836338f0d48938b9ff66
星雨月音 <162592269+StarRainer@users.noreply.github.com>
2025-12-28 16:06:01 +0800

    Feat/backend (#2)
    * [feat]:Finish Server and Client Network.

--- server/main.cpp
+++ server/main.cpp
@@ -1,9 +1,15 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+#include "server.h"
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    Server* server = Server::getInstance();
+    server->start(5523);
+
     MainWindow w;
     w.show();
 
---
+++ server/server.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QMap>
+#include <QJsonObject>
+#include <QJsonDocument>
+#include <QObject>
+#include <QDebug>
+
+// 用户会话信息结构
+struct UserSession {
+    int userId;
+    QString username;
+};
+
+class Server : public QTcpServer {
+    Q_OBJECT
+public:
+    static Server* getInstance();
+
+    // 启动服务端，绑定端口
+    bool start(quint16 port);
+
+protected:
+    void incomingConnection(qintptr socketDescriptor) override;
+
+private slots:
+    void readData();
+
+    void disconnected();
+
+private:
+    QMap<QTcpSocket*, UserSession> userMap;
+
+    static Server* server;
+
+    explicit Server(QObject* parent = nullptr);
+
+    void processRequest(QTcpSocket* socket, const QJsonObject& request);
+
+    void sendJson(QTcpSocket* socket, const QJsonObject& obj);
+};
+
